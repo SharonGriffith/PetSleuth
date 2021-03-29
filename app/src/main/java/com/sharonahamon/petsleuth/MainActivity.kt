6427d5f029e1882e43aaac21e4eefcf10c147113
Sharon A. Hamon <31077791+SharonAHamon@users.noreply.github.com>
2021-03-28 19:32:20 -0600

    debug logging

--- app/src/main/java/com/sharonahamon/petsleuth/MainActivity.kt
+++ app/src/main/java/com/sharonahamon/petsleuth/MainActivity.kt
@@ -8,7 +8,48 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.i("called OnCreate")
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("called OnDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Timber.i("called OnSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Timber.i("called OnRestoreInstanceState")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("called OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("called OnPause")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("called OnStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("called OnRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("called OnStop")
     }
 }
