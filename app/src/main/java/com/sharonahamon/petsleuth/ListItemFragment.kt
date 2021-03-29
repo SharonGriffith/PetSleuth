b69f4e56e8ea5cfae4af47869abee927214fad4e
Sharon A. Hamon <31077791+SharonAHamon@users.noreply.github.com>
2021-03-29 01:23:45 -0600

    Revert "flatten view model"
    This reverts commit 4e3507444547656253a1eec985b834f4ee0bb289.

--- app/src/main/java/com/sharonahamon/petsleuth/ListItemFragment.kt
+++ app/src/main/java/com/sharonahamon/petsleuth/ListItemFragment.kt
@@ -34,6 +34,20 @@ class ListItemFragment : Fragment() {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.list_item_fragment, container, false)
 
+        viewModel.appUser = AppUser("sharon.a.hamon@gmail.com")
+        viewModel.contactPerson =
+            ContactPerson("sharon.a.hamon@gmail.com", "Sharon", "Hamon", 3036677720)
+        viewModel.petDetail =
+            PetDetail(1, "blue point siamese tortie", "Casper", true, viewModel.appUser, null)
+        viewModel.petSummary = PetSummary(
+            1,
+            "Cat",
+            PetLastSeenLocation(1, "3/28/21", "Main Street", "Thornton", "CO", "80602"),
+            isMine = true,
+            isReunited = false,
+            status = "Lost"
+        )
+
         binding.petSleuthViewModel = viewModel
         binding.lifecycleOwner = this
 
--- app/src/main/java/com/sharonahamon/petsleuth/PetSleuthViewModel.kt
+++ app/src/main/java/com/sharonahamon/petsleuth/PetSleuthViewModel.kt
@@ -1,42 +1,20 @@
 package com.sharonahamon.petsleuth
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sharonahamon.petsleuth.models.AppUser
+import com.sharonahamon.petsleuth.models.ContactPerson
+import com.sharonahamon.petsleuth.models.PetDetail
+import com.sharonahamon.petsleuth.models.PetSummary
 import timber.log.Timber
 
 class PetSleuthViewModel : ViewModel() {
-    private var _email = MutableLiveData<String>()
-    val email: LiveData<String>
-        get() = _email
+    lateinit var appUser: AppUser
 
-    private var _description = MutableLiveData<String>()
-    val description: LiveData<String>
-        get() = _description
+    lateinit var contactPerson: ContactPerson
 
-    private var _species = MutableLiveData<String>()
-    val species: LiveData<String>
-        get() = _species
+    lateinit var petDetail: PetDetail
 
-    private var _lastSeenDate = MutableLiveData<String>()
-    val lastSeenDate: LiveData<String>
-        get() = _lastSeenDate
-
-    private var _city = MutableLiveData<String>()
-    val city: LiveData<String>
-        get() = _city
-
-    private var _state = MutableLiveData<String>()
-    val state: LiveData<String>
-        get() = _state
-
-    private var _zip = MutableLiveData<String>()
-    val zip: MutableLiveData<String>
-        get() = _zip
-
-    private var _status = MutableLiveData<String>()
-    val status: LiveData<String>
-        get() = _status
+    lateinit var petSummary: PetSummary
 
     init {
         Timber.i("ViewModel created")
@@ -45,16 +23,5 @@ class PetSleuthViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Timber.i("ViewModel destroyed")
-    }
-
-    fun loadMockData() {
-        _email = MutableLiveData("sharon.a.hamon@gmail.com")
-        _description = MutableLiveData("blue point siamese tortie")
-        _species = MutableLiveData("Cat")
-        _lastSeenDate = MutableLiveData("3/28/21")
-        _city = MutableLiveData("Thornton")
-        _state = MutableLiveData("CO")
-        _zip = MutableLiveData("80602")
-        _status = MutableLiveData("Lost")
     }
 }
