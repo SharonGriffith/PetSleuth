3ccdb91c78a3b7f66b7f7b9ea548afee8acc5f40
Sharon A. Hamon <31077791+SharonAHamon@users.noreply.github.com>
2021-04-05 16:53:54 -0600

    delete unneeded code

--- app/src/main/java/com/sharonahamon/petsleuth/screens/list/PetCardFragment.kt
+++ app/src/main/java/com/sharonahamon/petsleuth/screens/list/PetCardFragment.kt
@@ -8,16 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
 import com.sharonahamon.petsleuth.R
 import com.sharonahamon.petsleuth.databinding.PetCardFragmentBinding
-import com.sharonahamon.petsleuth.common.PetSleuthViewModel
 import timber.log.Timber
 
 class PetCardFragment : Fragment() {
-
-    private lateinit var viewModel: PetSleuthViewModel
 
     private lateinit var binding: PetCardFragmentBinding
 
@@ -27,74 +22,9 @@ class PetCardFragment : Fragment() {
     ): View {
         Timber.i("called OnCreateView")
 
-        // get the existing instance of the viewModel instead of creating a new one
-        // tie the viewModel to the parent activity so that it does not get
-        // destroyed when a fragment is popped off the back stack
-        viewModel = ViewModelProvider(requireActivity()).get(PetSleuthViewModel::class.java)
-        Timber.i("called ViewModelProvider")
-
         binding = DataBindingUtil.inflate(inflater, R.layout.pet_card_fragment, container, false)
-
-        binding.petSleuthViewModel = viewModel
         binding.lifecycleOwner = this
 
-        binding.listItemImage.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_listFragment_to_detailFragment))
-
         return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Timber.i("called OnDestroyView")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Timber.i("called OnCreate")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("called OnDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Timber.i("called OnDetach")
-    }
-
-    override fun onInflate(context: Context, attrs: AttributeSet, savedInstanceState: Bundle?) {
-        super.onInflate(context, attrs, savedInstanceState)
-        Timber.i("called OnInflate")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.i("called OnPause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.i("called OnResume")
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        Timber.i("called OnSaveInstanceState")
-    }
-
-    override fun onAttachFragment(childFragment: Fragment) {
-        super.onAttachFragment(childFragment)
-        Timber.i("called OnAttachFragment")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Timber.i("called OnStart")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("called OnStop")
     }
 }
