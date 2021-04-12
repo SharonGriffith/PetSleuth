90ce02cd94c5002c81a6da9e8eb97f501f5e5870
Sharon A. Hamon <31077791+SharonAHamon@users.noreply.github.com>
2021-04-12 15:10:35 -0600

    use a drawable image in the adapter

--- app/src/main/java/com/sharonahamon/petsleuth/screens/list/PetCardListViewAdapter.kt
+++ app/src/main/java/com/sharonahamon/petsleuth/screens/list/PetCardListViewAdapter.kt
@@ -3,10 +3,12 @@ package com.sharonahamon.petsleuth.screens.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.sharonahamon.petsleuth.R
+import com.sharonahamon.petsleuth.common.SpeciesTypeEnum
 import com.sharonahamon.petsleuth.data.PetLD
 
 /**
@@ -33,7 +35,16 @@ class PetCardListViewAdapter(
         fun bind(
             position: Int
         ) {
+            //val res = itemView.context.resources
+
             val pet = petList[position]
+
+            // https://gist.github.com/garuma/f0f1662a6587e2c8d916
+            petImage.setImageResource(when (pet.value?.petSummary?.value?.species?.value.toString()) {
+                SpeciesTypeEnum.CAT.species -> R.drawable.cat_face_vector
+                //SpeciesTypeEnum.DOG.species -> R.drawable.ic_dog
+                else -> R.drawable.ic_add
+            })
 
             petId.text = "Pet ID: " + pet.value?.petId?.value.toString()
             lastSeenDate.text =
@@ -44,6 +55,7 @@ class PetCardListViewAdapter(
                 pet.value?.petLastSeenLocation?.value?.city?.value.toString() + " " + pet.value?.petLastSeenLocation?.value?.state?.value.toString() + " " + pet.value?.petLastSeenLocation?.value?.zip?.value.toString()
         }
 
+        val petImage: ImageView = view.findViewById(R.id.list_item_image)
         val petId: TextView = view.findViewById(R.id.list_item_number)
         val lastSeenDate: TextView = view.findViewById(R.id.list_item_date_value)
         val status: TextView = view.findViewById(R.id.list_item_status_value)
